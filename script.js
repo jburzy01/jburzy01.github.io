f149b62fc247e0ee3bd29b7c9d12ed1204e276f4
jburzy01 <jacksonburzynski@gmail.com>
2016-02-29 17:19:00 -0500

    test

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>space</a></li>" +
-"<li><a href='cubes.html'>cubes</a></li>" +
+"<li><a href='test.html'>cubes</a></li>" +
 "</ul>";
